3b9aee12ace6640e23d8173ba6923556ee1e0788
Rayyan Emir Muhammad <rayyan.emir@ui.ac.id>
2026-05-13 19:35:48 +0700

    betulin yang tk04 (ver 1)

--- member/urls.py
+++ member/urls.py
@@ -2,30 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Fitur 7 – CRUD Manajemen Identitas (Member)
     path('identitas/',                      views.identitas_list,   name='identitas_list'),
     path('identitas/tambah/',               views.identitas_tambah, name='identitas_tambah'),
     path('identitas/<str:nomor>/edit/',     views.identitas_edit,   name='identitas_edit'),
     path('identitas/<str:nomor>/hapus/',    views.identitas_hapus,  name='identitas_hapus'),
 
-    # Fitur 8 - Claim Missing Miles
     path('klaim/', views.klaim_list, name='klaim_list'),
     path('klaim/buat/', views.klaim_buat, name='klaim_buat'),
     path('klaim/edit/<int:id>/', views.klaim_edit, name='klaim_edit'),
     path('klaim/hapus/<int:id>/', views.klaim_hapus, name='klaim_hapus'),
 
-    # Fitur 9 - Redeem Hadiah
     path('redeem/', views.redeem_list, name='redeem_list'),
     path('redeem/<str:kode_hadiah>/', views.redeem_buat, name='redeem_buat'),
 
-    # Fitur 10 - Pembelian Award Miles Package
     path('package/', views.package_list, name='package_list'),
     path('package/<str:package_id>/', views.package_beli, name='package_beli'),
 
-    # Fitur 11 - Informasi Tier
     path('tier/', views.tier_info, name='tier_info'),
 
-    # Fitur 10 - Transfer Miles
     path('transfer/', views.transfer_list, name='transfer_list'),
     path('transfer/buat/', views.transfer_buat, name='transfer_buat'),
 ]
--- staf/urls.py
+++ staf/urls.py
@@ -2,25 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Fitur 6 – CRUD Manajemen Data Member (Staf)
     path('kelola-member/',                  views.member_list,   name='member_list_staf'),
     path('kelola-member/tambah/',           views.member_tambah, name='member_tambah'),
     path('kelola-member/<str:email>/edit/', views.member_edit,   name='member_edit'),
     path('kelola-member/<str:email>/hapus/',views.member_hapus,  name='member_hapus'),
 
-    # Fitur 9 - Kelola Klaim Missing Miles (Staf)
     path('kelola-klaim/', views.klaim_list_staf, name='kelola_klaim'),
     path('kelola-klaim/<int:id>/proses/', views.klaim_proses, name='klaim_proses'),
     path('laporan/', views.laporan_transaksi, name='laporan_transaksi'),
     path('laporan/hapus/', views.laporan_transaksi_hapus, name='laporan_transaksi_hapus'),
 
-    # Fitur 16 - CRUD Manajemen Mitra
     path('kelola-mitra/', views.mitra_list, name='mitra_list'),
     path('kelola-mitra/create/', views.mitra_create, name='mitra_create'),
     path('kelola-mitra/update/<str:email>/', views.mitra_update, name='mitra_update'),
     path('kelola-mitra/delete/<str:email>/', views.mitra_delete, name='mitra_delete'),
 
-    # Fitur 17 - CRUD Manajemen Hadiah & Penyedia
     path('kelola-hadiah/', views.hadiah_list, name='hadiah_list'),
     path('kelola-hadiah/create/', views.hadiah_create, name='hadiah_create'),
     path('kelola-hadiah/update/<str:kode_hadiah>/', views.hadiah_update, name='hadiah_update'),
